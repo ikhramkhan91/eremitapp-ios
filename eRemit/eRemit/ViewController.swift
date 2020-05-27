de99850bd8baee5188452a08afc9564e828c2173
Mohammed Rizwan <rizu_93@yahoo.in>
2020-05-27 04:51:59 +0300

    Project created

---
+++ eRemit/eRemit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eRemit
+//
+//  Created by Mohammed Rizwan on 27/05/20.
+//  Copyright © 2020 DreamSwitch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
